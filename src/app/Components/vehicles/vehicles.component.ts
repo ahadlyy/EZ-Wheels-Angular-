fe28750c864c85502357f908c05da129ba3665a3
DavidAyad97 <davidayad80@gmail.com>
2024-05-08 17:28:58 +0300

    car rent service and interface for car and car rent

---
+++ src/app/Components/vehicles/vehicles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicles',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicles.component.html',
+  styleUrl: './vehicles.component.css'
+})
+export class VehiclesComponent {
+
+}
