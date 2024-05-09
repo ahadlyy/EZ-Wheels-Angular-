e50c95fa906b07e56e7cd1119cfccbddff0fc3ee
mohamed abdou <modohoda2468@gmail.com>
2024-05-09 18:55:11 +0300

    footer without colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FooterComponent } from './Core/Footer/footer/footer.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Core/Header/header/header.component';
@@ -5,7 +6,7 @@ import { HeaderComponent } from './Core/Header/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
