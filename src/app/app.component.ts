bb9814daa80f27869698cf6a0b991c3ddffef0c1
AhmedAdly <157370741+ahadlyy@users.noreply.github.com>
2024-05-09 18:52:12 +0300

    home.component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Core/Header/header/header.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
